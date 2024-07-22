b7a9edb7af5e3e0d1c082fcaa63d8b0062a491c3
alditpra <49697756+alditpra@users.noreply.github.com>
2024-07-22 11:23:38 +0700

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -115,7 +115,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Hubungi Kami', href: 'https://google.com', target: '_blank' }],
 };
 
 export const footerData = {
